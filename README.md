3de2b69272579ee4f6159e2da5ea96c69e42544b
nodepi <node.raspberry@gmail.com>
2015-08-31 15:56:14 +0000

    gh-pages creation

--- README.md
+++
@@ -1,2 +0,0 @@
-# nodejs_pi2
-compilation of nodejs sources for raspberry pi 2
--- build.sh
+++
@@ -1,9 +0,0 @@
-# command line for building the node js
-
-# use nvm to select version
-# build sources with nvm
-
-# copy builded sources in node/ and npm/ directory
-# make a tar.gz in dist directory
-tar -cvzf  dist/node_0.12.1.tar.gz node/ npm/
-
